fd2d60cb76e0c46083740e7b099bd8e0e09116ea
nnaveen20242-cpu <nnaveen20242@gmail.com>
2026-04-13 21:14:03 +0530

    Added UC7: String.join for greeting

---
+++ HellooApp.java
@@ -0,0 +1,11 @@
+public class HellooApp {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
