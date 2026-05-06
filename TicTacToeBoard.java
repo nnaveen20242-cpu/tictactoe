cec99e9c373d429da026aa60804f5e5edd8994c5
nnaveen20242-cpu <nnaveen20242@gmail.com>
2026-05-06 20:43:24 +0530

    Update and rename HelloWorld.java to TicTacToeBoard.java

---
+++ TicTacToeBoard.java
@@ -0,0 +1,26 @@
+public class TicTacToeBoard {
+
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        initializeBoard(board);
+        printBoard(board);
+    }
+
+    public static void initializeBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
